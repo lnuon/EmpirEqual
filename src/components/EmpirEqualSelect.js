0df3fe0b556017b389fa5e5ec812c5e909542adc
lnispel <lnispel@me.com>
2018-06-09 15:32:25 -0500

    file structure

---
+++ src/components/EmpirEqualSelect.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import Select from 'react-select';
+import 'react-select/dist/react-select.css';
+
+export default class EmpriEqualSelect extends React.Component {
+  handleChange = value => {
+    // this is going to call setFieldValue and manually update values.topcis
+    this.props.onChange(this.props.name, value);
+  };
+
+  handleBlur = () => {
+    // this is going to call setFieldTouched and manually update touched.topcis
+    this.props.onBlur(this.props.name, true);
+  };
+
+  render() {
+    return (
+      <div style={{ margin: '1rem 0' }}>
+        <Select
+          id="color"
+          options={options}
+          multi={true}
+          onChange={this.handleChange}
+          onBlur={this.handleBlur}
+          value={this.props.value}
+        />
+        {!!this.props.error &&
+        this.props.touched && (
+          <div style={{ color: 'red', marginTop: '.5rem' }}>
+            {this.props.error}
+          </div>
+        )}
+      </div>
+    );
+  }
+}
---
+++ src/components/SignUp.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import EmpirEqualSelect from './EmpirEqualSelect';
+
+class SignUp extends Component {
+  render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
+
+export default SignUp;
