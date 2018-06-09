7572d1cf698646b2fddfaec242515fd75f5b2ff2
Javier <javier.williams720@gmail.com>
2018-06-09 16:18:08 -0500

    setup basic homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,35 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
+
+const HomepageContainer = styled.div`
+	display: flex;
+	flex-direction: column;
+`;
+
+const HeroSection = styled.section`
+	text-align: center;
+`;
+
+const HeroTitle = styled.h1`
+	font-size: 24px;
+`
+
+const CallToAction = styled.button`
+`;
+
 
 class Home extends Component {
   render() {
     return (
-      <div>
-      </div>
+      <HomepageContainer>
+				<HeroSection>
+					<HeroTitle>EmpirEqual</HeroTitle>
+					<Link to="/enter">
+						<CallToAction>Try it out!</CallToAction>
+					</Link>
+				</HeroSection>
+      </HomepageContainer>
     );
   }
 }
